0a069dd49ae068fdc989a4d026de879c39e38429
jurom <jurajmchc@gmail.com>
2014-05-22 11:47:17 +0200

    levels added

--- lib/logger.dart
+++ lib/logger.dart
@@ -14,28 +14,74 @@ String logToJson(Map log) => JSON.encode(log,
       return res;
     });
 
-class Logger {
+class Level implements Comparable<Level> {
 
-  static const int ALL = 0;
-  static const int FINEST = 300;
-  static const int FINER = 400;
-  static const int FINE = 500;
-  static const int CONFIG = 700;
-  static const int INFO = 800;
-  static const int WARNING = 900;
-  static const int SEVERE = 1000;
-  static const int SHOUT = 1200;
-  static const int OFF = 2000;
+  final String name;
+
+  /**
+   * Unique value for this level. Used to order levels, so filtering can exclude
+   * messages whose level is under certain value.
+   */
+  final int value;
+
+  const Level(this.name, this.value);
+
+  /** Special key to turn on logging for all levels ([value] = 0). */
+  static const Level ALL = const Level('ALL', 0);
+
+  /** Special key to turn off all logging ([value] = 2000). */
+  static const Level OFF = const Level('OFF', 2000);
+
+  /** Key for highly detailed tracing ([value] = 300). */
+  static const Level FINEST = const Level('FINEST', 300);
+
+  /** Key for fairly detailed tracing ([value] = 400). */
+  static const Level FINER = const Level('FINER', 400);
+
+  /** Key for tracing information ([value] = 500). */
+  static const Level FINE = const Level('FINE', 500);
+
+  /** Key for static configuration messages ([value] = 700). */
+  static const Level CONFIG = const Level('CONFIG', 700);
+
+  /** Key for informational messages ([value] = 800). */
+  static const Level INFO = const Level('INFO', 800);
+
+  /** Key for potential problems ([value] = 900). */
+  static const Level WARNING = const Level('WARNING', 900);
+
+  /** Key for serious failures ([value] = 1000). */
+  static const Level SEVERE = const Level('SEVERE', 1000);
+
+  /** Key for extra debugging loudness ([value] = 1200). */
+  static const Level SHOUT = const Level('SHOUT', 1200);
+
+  static const List<Level> LEVELS = const
+      [ALL, FINEST, FINER, FINE, CONFIG, INFO, WARNING, SEVERE, SHOUT, OFF];
+
+  bool operator ==(Object other) => other is Level && value == other.value;
+  bool operator <(Level other) => value < other.value;
+  bool operator <=(Level other) => value <= other.value;
+  bool operator >(Level other) => value > other.value;
+  bool operator >=(Level other) => value >= other.value;
+  int compareTo(Level other) => value - other.value;
+  int get hashCode => value;
+  String toString() => name;
+  toJson() => {"name":name, "value":value};
+
+}
+
+class Logger {
 
   final source;
   Function getMetaData;
-  int logLevel = null;
+  Level logLevel = null;
   Logger _parent;
   static StreamController _streamController = new StreamController.broadcast();
 
   shouldLog(level) => logLevel == null ? _parent.shouldLog(level) : logLevel <= level;
 
-  log(int level, String event, {dynamic data, error, stackTrace}) {
+  log(Level level, String event, {dynamic data, error, stackTrace}) {
     if (shouldLog(level)) {
       _streamController.add({
         'level':level,
@@ -51,28 +97,28 @@ class Logger {
   }
 
   info(String event, {dynamic data, error, stackTrace})
-    => log(INFO, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.INFO, event, data:data, error:error, stackTrace:stackTrace);
 
   warning(String event, {dynamic data, error, stackTrace})
-    => log(WARNING, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.WARNING, event, data:data, error:error, stackTrace:stackTrace);
 
   severe(String event, {dynamic data, error, stackTrace})
-    => log(SEVERE, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.SEVERE, event, data:data, error:error, stackTrace:stackTrace);
 
   shout(String event, {dynamic data, error, stackTrace})
-    => log(SHOUT, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.SHOUT, event, data:data, error:error, stackTrace:stackTrace);
 
   config(String event, {dynamic data, error, stackTrace})
-    => log(CONFIG, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.CONFIG, event, data:data, error:error, stackTrace:stackTrace);
 
   fine(String event, {dynamic data, error, stackTrace})
-    => log(FINE, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.FINE, event, data:data, error:error, stackTrace:stackTrace);
 
   finer(String event, {dynamic data, error, stackTrace})
-    => log(FINER, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.FINER, event, data:data, error:error, stackTrace:stackTrace);
 
   finest(String event, {dynamic data, error, stackTrace})
-    => log(FINEST, event, data:data, error:error, stackTrace:stackTrace);
+    => log(Level.FINEST, event, data:data, error:error, stackTrace:stackTrace);
 
   /**
    * {
@@ -87,7 +133,7 @@ class Logger {
   static final Map<String, Logger> _loggers = {};
 
   static _createRootLogger() =>
-    new Logger._internal("", null)..logLevel = Logger.WARNING;
+    new Logger._internal("", null)..logLevel = Level.WARNING;
 
   /** Root logger. */
   static Logger get ROOT => new Logger('');
