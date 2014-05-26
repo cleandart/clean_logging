6b7769b753068f33563bf244d0d89c363cdd0c30
jurom <jurajmchc@gmail.com>
2014-05-26 13:02:13 +0200

    changes made according to comments

--- lib/http_handler.dart
+++ lib/http_handler.dart
@@ -21,13 +21,11 @@ abstract class HttpHandler {
     if (_running) return;
     if (_queue.isEmpty) return;
     _running = true;
-    // Items might be added to queue while we are trying to send it
-    List dataToSend = new List.from(_queue);
-    _queue.removeRange(0,dataToSend.length);
-    send(encode(dataToSend)).then((_) {
+    send(encode(_queue)).then((_) {
       _running = false;
       // Keep performing until the queue is empty
       _batchSend();
     });
+    _queue.clear();
   }
 }
