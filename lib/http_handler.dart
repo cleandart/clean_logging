b72ee494128d3c24b0f11efdab5d2a2b294f5cf4
jurom <jurajmchc@gmail.com>
2014-07-15 13:39:37 +0200

    prints

--- lib/http_handler.dart
+++ lib/http_handler.dart
@@ -13,19 +13,31 @@ abstract class HttpHandler {
 
   handleData(data) {
     _queue.add(data);
+    print("Queue: $_queue");
     _batchSend();
   }
 
   _batchSend() {
     // There shall be only one _batchSend function running at a time per handler instance
+    print("Running ? $_running");
+    print("Is Empty ? ${_queue.isEmpty}");
     if (_running) return;
     if (_queue.isEmpty) return;
     _running = true;
-    send(encode(_queue)).then((_) {
+    print("Queue before send: $_queue");
+    var enc;
+    try {
+      enc = encode(_queue);
+    } catch (e) {
+      print("Error in encoding: $e");
+    }
+    print("Trying to send: ${enc}");
+    send(enc).then((_) {
+      print("Sent: $_queue");
       _running = false;
       // Keep performing until the queue is empty
       _batchSend();
-    });
+    }).catchError((e,s) => print("ERROR: $e $s"));
     _queue.clear();
   }
 }
