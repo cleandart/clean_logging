533a6d58c1f58e3e6b742a43eb3032c52134640d
Marcela Hrda <aklecram@gmail.com>
2014-10-02 17:32:46 +0200

    Logging will not in infinite loop in case someone logs during logging.

---
+++ test/logger.dart
@@ -0,0 +1,23 @@
+library clean_logging.test_logger;
+
+import 'package:unittest/unittest.dart';
+import 'package:clean_logging/logger.dart';
+
+Logger l = new Logger('pokus');
+
+void main() {
+  test('Logging inside logging does not end in infinite loop.', () {
+    // given
+    Logger.ROOT.logLevel = Level.ALL;
+    Logger.onRecord.listen((_) => handleLog(_));
+    l.info("Hello!");
+  });
+}
+
+handleLog(lr) {
+  try {
+    throw new Exception("Exception");
+  } catch (e) {
+    l.shout("$e");
+  }
+}
